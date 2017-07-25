ab934690b3c68970bdbb3d19c6a9ac9e22faff09
Herr-Gunter <kubyshkin1990@mail.ru>
2017-07-25 16:33:35 +0300

    25.07.2017.1

--- app/js/common.js
+++ app/js/common.js
@@ -2,4 +2,34 @@ $(function() {
 
 	// Custom JS
 
+
+	//trigger submenu
+
+	var $li = $('.top-nav ul li:nth-child(2)');
+
+	$li.click(function (){
+		var $sub = $('.embeded-nav');
+		if ($sub.hasClass('embeded-nav-active')){
+			$sub.removeClass('embeded-nav-active');
+			$li.removeClass('li-active');
+			$sub.fadeOut();
+		}else{
+			$sub.fadeIn();
+			$li.addClass('li-active');
+			
+			$sub.addClass('embeded-nav-active');
+		}
+	});
+
+//===================================SLIDER============================//
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+  	loop: true,
+  	items: 1,
+  	//autoWidth:true
+  });
+});
+
+
 });
